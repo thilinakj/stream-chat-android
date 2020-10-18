7c241879d58c7e20203ee12cc791644f48180ac1
Carter Hudson <carter@getstream.io>
2020-10-18 17:18:11 -0600

    fix ensure method

--- library/src/main/java/com/getstream/sdk/chat/view/common/Extensions.kt
+++ library/src/main/java/com/getstream/sdk/chat/view/common/Extensions.kt
@@ -1,5 +1,6 @@
 package com.getstream.sdk.chat.view.common
 
+import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
 import android.view.View
@@ -15,7 +16,8 @@ fun View.visible(isVisible: Boolean) {
 /**
  * Ensures the context being accessed in a View can be cast to Activity
  */
-internal fun Context.ensure() = when (this) {
+internal fun Context.ensure(): Context = when (this) {
+    is Activity -> this
     is ContextWrapper -> baseContext
     else -> this
 }
